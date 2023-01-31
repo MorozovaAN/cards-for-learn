da5593fc37b7a0bc746a7928208fa720aadbbf40
lily1215z <odessa1215@i.ua>
2023-01-31 18:21:15 +0100

    feat: add checkBox in StoryBook

---
+++ src/UI/checkbox/Checkbox.stories.tsx
@@ -0,0 +1,19 @@
+import 'index.css'
+
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+
+import { Checkbox } from './Checkbox'
+
+export default {
+  title: 'Checkbox',
+  component: Checkbox,
+} as ComponentMeta<typeof Checkbox>
+
+const Template: ComponentStory<typeof Checkbox> = args => <Checkbox {...args} />
+
+export const Default = Template.bind({})
+
+Default.args = {
+  checked: false,
+  disabled: false,
+}
