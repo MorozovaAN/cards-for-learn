db619949194e47e4f2613a2599b536f5d2237f48
Morozova Anastasiya <MorozovaAnastasiyaN@yandex.ru>
2023-02-01 21:04:23 +0400

    feat: add authApi and me request

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,14 @@
+import { useEffect } from 'react'
+
+import { useMeMutation } from '../modules/auth/authApi'
+
 import s from 'app/App.module.scss'
 export const App = () => {
-  return <></>
+  const [initializeApp, { isLoading, error }] = useMeMutation()
+
+  useEffect(() => {
+    initializeApp({}).unwrap()
+  }, [])
+
+  return isLoading ? <h2>loading</h2> : <div>success</div>
 }
