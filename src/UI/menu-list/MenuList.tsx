d5d9b6cea33075399443fc75ad577f7eb77b9c89
Morozova Anastasiya <MorozovaAnastasiyaN@yandex.ru>
2023-02-08 22:16:59 +0400

    style: change loader circular color

--- src/UI/menu-list/MenuList.tsx
+++ src/UI/menu-list/MenuList.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactNode } from 'react'
+import React, { FC } from 'react'
 
 import { Navigate, useNavigate } from 'react-router-dom'
 
@@ -9,58 +9,54 @@ import { MenuType } from 'modules/packs/Packs'
 import { PATH } from 'routes/routes'
 import s from 'UI/menu-list/MenuList.module.scss'
 
-type MenuListType = {
-  children: ReactNode
-  open: boolean
+type MenuProps = {
+  menu: MenuType[]
 }
-export const MenuList: FC<MenuListType> = ({ children, open }) => {
+export const MenuList: FC<MenuProps> = menu => {
   const [logout] = useLogOutMutation()
   const isLoggedIn = useTypedSelector(isLoggedInSelector)
   const navigate = useNavigate()
 
-  if (!open) return null
+  const handleMenuItem = (id: number) => {
+    switch (id) {
+      case 1:
+        alert('modal Edit')
+        break
+      case 2:
+        alert('modal Delete')
+        break
+      case 3:
+        alert('modal learn')
+        break
+      case 4:
+        navigate(PATH.PROFILE)
+        break
+      case 5:
+        logout()
+        break
+      case 6:
+        alert('modal packs')
+        break
+      case 7:
+        alert('modal cards')
+        break
+      default:
+        alert("Don't have value")
+    }
+  }
 
-  return <div className={s.menuList}>{children}</div>
-  // const handleMenuItem = (id: number) => {
-  //   switch (id) {
-  //     case 1:
-  //       alert('modal Edit')
-  //       break
-  //     case 2:
-  //       alert('modal Delete')
-  //       break
-  //     case 3:
-  //       alert('modal learn')
-  //       break
-  //     case 4:
-  //       navigate(PATH.PROFILE)
-  //       break
-  //     case 5:
-  //       logout()
-  //       break
-  //     case 6:
-  //       alert('modal packs')
-  //       break
-  //     case 7:
-  //       alert('modal cards')
-  //       break
-  //     default:
-  //       alert("Don't have value")
-  //   }
-  // }
-  //
-  // if (!isLoggedIn) {
-  //   return <Navigate to={PATH.LOG_IN} />
-  // }
+  if (!isLoggedIn) {
+    return <Navigate to={PATH.LOG_IN} />
+  }
 
-  // const menuListData = menu.menu.map((i: MenuType) => {
-  //   return (
-  //     <li className={s.item} key={i.id} onClick={() => handleMenuItem(i.id)}>
-  //       <img src={i.img} alt="icon" />
-  //       <h3 className={s.title}>{i.title}</h3>
-  //     </li>
-  //   )
-  // })
-  //
-  // return <ul className={s.items}>{menuListData}</ul>
+  const menuListData = menu.menu.map((i: MenuType) => {
+    return (
+      <li className={s.item} key={i.id} onClick={() => handleMenuItem(i.id)}>
+        <img src={i.img} alt="icon" />
+        <h3 className={s.title}>{i.title}</h3>
+      </li>
+    )
+  })
+
+  return <ul className={s.items}>{menuListData}</ul>
 }
--- src/modules/header/Header.tsx
+++ src/modules/header/Header.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import s from './Header.module.scss'
 
@@ -9,14 +9,21 @@ import profileIcon from 'assets/img/icons/profile-user.svg'
 import logo from 'assets/img/logo.svg'
 import avatarPlug from 'assets/img/user-avatar-default.svg'
 import { useTypedSelector } from 'common/hooks/useTypedSelector'
-import { useLogOutMutation } from 'modules/auth/authApi'
 import { avatarSelector, nameSelector } from 'modules/auth/authSelectors'
 import { PATH } from 'routes/routes'
 import { MenuList } from 'UI/menu-list/MenuList'
 export const Header = () => {
+  const [toggleV, setToggleVP] = useState(false)
   const userName = useTypedSelector(nameSelector)
   const userAvatar = useTypedSelector(avatarSelector)
   const avatar = userAvatar ? userAvatar : avatarPlug
+
+  const menuListInProfile = [
+    { img: profileIcon, title: 'Profile', id: 4 },
+    { img: profileIcon, title: 'Packs', id: 6 },
+    { img: profileIcon, title: 'Cards', id: 7 },
+    { img: profileLogout, title: 'Log out', id: 5 },
+  ]
 
   return (
     <header className={s.headerContainer}>
@@ -25,14 +32,10 @@ export const Header = () => {
           <img src={logo} alt="logo" />
         </Link>
 
-        <div className={s.userInfoContainer}>
+        <div className={s.userInfoContainer} onClick={() => setToggleVP(!toggleV)}>
           <p className={s.name}>{userName}</p>
-          <img
-            src={avatar}
-            alt="user avatar"
-            className={s.avatar}
-            onClick={() => setOpenMenu(!openMenu)}
-          />
+          <img src={avatar} alt="user avatar" className={s.avatar} />
+          <div className={s.menuListBox}>{toggleV && <MenuList menu={menuListInProfile} />}</div>
         </div>
       </nav>
     </header>
