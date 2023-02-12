166818bd66848cd39e2be08cb2ec699170c80b99
lily1215z <odessa1215@i.ua>
2023-02-12 17:30:16 +0100

    feat: add pack

--- src/modules/packs/packsApi.ts
+++ src/modules/packs/packsApi.ts
@@ -1,10 +1,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react'
 
+import { setIsLoading, setIsLoggedIn, setNotification } from 'app/appSlice'
 import { baseURL } from 'common/constants/base-URL'
 import { BaseQueryParamsType } from 'common/constants/baseQueryParams'
+import { ErrorType } from 'common/types/types'
+import { setAuthData } from 'modules/auth/authSlice'
 
 export const packsApi = createApi({
   reducerPath: 'packsApi',
+  tagTypes: ['packs'],
   baseQuery: fetchBaseQuery({
     baseUrl: `${baseURL}/cards/`,
     credentials: 'include',
@@ -18,13 +22,49 @@ export const packsApi = createApi({
           ...arg,
         },
       }),
+      providesTags: result =>
+        result
+          ? [
+              ...result.cardPacks.map(({ _id }) => ({ type: 'packs' as const, _id })),
+              { type: 'packs', id: 'LIST' },
+            ]
+          : [{ type: 'packs', id: 'LIST' }],
+    }),
+    addPack: build.mutation<ResponseNewPack, AddPackType>({
+      query: body => ({
+        url: 'pack',
+        method: 'POST',
+        body,
+      }),
+      async onQueryStarted(_, { dispatch, queryFulfilled }) {
+        try {
+          await queryFulfilled
+        } catch (err) {
+          if ((err as ErrorType)?.error?.data?.error) {
+            dispatch(setNotification({ message: 'Incorrect email or password', type: 'error' }))
+          } else {
+            dispatch(setNotification({ message: 'Something went wrong', type: 'error' }))
+          }
+        } finally {
+          dispatch(setIsLoading(false))
+        }
+      },
+      invalidatesTags: [{ type: 'packs', id: 'LIST' }],
     }),
   }),
 })
-export const { useGetPacksQuery } = packsApi
+export const { useGetPacksQuery, useAddPackMutation } = packsApi
 
 //types
-
+export type PackType = {
+  _id: string
+  user_id: string
+  user_name: string
+  name: string
+  cardsCount: number
+  created: string
+  updated: string
+}
 export type ResponsePacksType = {
   cardPacks: ResponsePackType[]
   cardPacksTotalCount: number
@@ -42,4 +82,14 @@ export type ResponsePackType = {
   cardsCount: number
   created: string
   updated: string
+}
+export type ResponseNewPack = {
+  newCardsPack: PackType
+}
+export type AddPackType = {
+  cardsPack: {
+    name: string
+    deckCover: string
+    private: boolean
+  }
 }
