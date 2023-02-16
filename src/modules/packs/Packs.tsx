ee311b3a794f46154bcd5f226a039afcb07f3b9f
Morozova Anastasiya <MorozovaAnastasiyaN@yandex.ru>
2023-02-16 17:07:18 +0400

    fix: move filtering to Packs component

--- src/modules/packs/Packs.tsx
+++ src/modules/packs/Packs.tsx
@@ -1,19 +1,29 @@
 import React, { FC } from 'react'
 
+import { useSearchParams } from 'react-router-dom'
+
+import { useTypedSelector } from 'common/hooks/useTypedSelector'
 import { formatDate } from 'common/utils/formatDate'
 import { MyPack } from 'components/packs/my-pack/MyPack'
 import { OtherPack } from 'components/packs/other-pack/OtherPack'
+import { PackType } from 'modules/packs/packsApi'
 
 type PacksType = {
-  packs: any //todo должно быть PackType[]
-  myPacks: boolean
+  responsePacks: any //todo должно быть PackType[]
 }
 
-export const Packs: FC<PacksType> = ({ packs, myPacks }) => {
+export const Packs: FC<PacksType> = ({ responsePacks }) => {
+  const [searchParams] = useSearchParams()
+  const myId = useTypedSelector(state => state.auth.id)
+  const myPacksPage = searchParams.has('user_id')
+  const packs = myPacksPage
+    ? responsePacks
+    : responsePacks.filter((p: PackType) => p.user_id !== myId)
+
   return packs.map((p: any) => {
     const dateUpdate = formatDate(p.updated)
 
-    return myPacks ? (
+    return myPacksPage ? (
       <MyPack
         key={p._id}
         packId={p._id}
