8a9dc29a80fccd729935f7a187ef96041807a01e
Oleg <9helgi9gmail.com>
2023-02-24 19:46:46 +0100

    feat: uploadImage.ts

---
+++ src/common/utils/uploadImage.ts
@@ -0,0 +1,38 @@
+import { ChangeEvent } from 'react'
+
+import { setNotification } from 'app/appSlice'
+import { AppDispatchType } from 'common/hooks/useTypedDispatch'
+import { convertFileToBase64 } from 'common/utils/toBase64'
+
+export const uploadImage = (
+  e: ChangeEvent<HTMLInputElement>,
+  dispatch: AppDispatchType,
+  callback: (file64: string) => void
+) => {
+  if (e.target.files && e.target.files.length) {
+    const file = e.target.files[0]
+
+    const extension = file.name.slice(-4)
+
+    if (file.size > 112000) {
+      dispatch(setNotification({ message: 'Sorry, max file size - 112 Kb!', type: 'error' }))
+    } else if (
+      extension !== '.jpg' &&
+      extension !== '.png' &&
+      extension !== '.svg' &&
+      extension !== 'jpeg'
+    ) {
+      dispatch(
+        setNotification({
+          message: 'Sorry, correct extensions .jpg, .png, .svg, .jpeg',
+          type: 'error',
+        })
+      )
+    } else {
+      convertFileToBase64(file, (file64: string) => {
+        callback(file64)
+      })
+    }
+  }
+  e.target.value = ''
+}
