c32965a607d52bc34ffe2fc62350bd7bdb999b88
Morozova Anastasiya <MorozovaAnastasiyaN@yandex.ru>
2023-03-01 21:56:36 +0400

    fix: NotFound

--- src/components/not-found/NotFound.tsx
+++ src/components/not-found/NotFound.tsx
@@ -3,15 +3,16 @@ import React, { FC } from 'react'
 import s from './NotFound.module.scss'
 
 type NotFoundType = {
-  notFound: string
+  notFound?: 'notFound'
 }
+
 export const NotFound: FC<NotFoundType> = ({ notFound }) => {
   return (
     <div className={s.text}>
       {notFound ? (
         <h2>Sorry we didn&apos;t find any results</h2>
       ) : (
-        <h2>You don&apos;t have any cards yet. </h2>
+        <h2>You don&apos;t have any cards yet</h2>
       )}
     </div>
   )
--- src/modules/cards/cards-list/CardsList.tsx
+++ src/modules/cards/cards-list/CardsList.tsx
@@ -9,6 +9,7 @@ import s from './CardsList.module.scss'
 import { skeletonsSelector } from 'app/appSelectors'
 import { useTypedSelector } from 'common/hooks/useTypedSelector'
 import { formatDate } from 'common/utils/formatDate'
+import { NotFound } from 'components/not-found/NotFound'
 import { Card } from 'modules/cards/card/Card'
 
 type CardsListType = {
@@ -16,38 +17,47 @@ type CardsListType = {
   myCards: boolean
   isFetching: boolean
 }
-
 export const CardsList: FC<CardsListType> = ({ cards, myCards, isFetching }) => {
   const skeletons = useTypedSelector(skeletonsSelector)
 
   return (
-    <div className={s.cardsList}>
-      <div className={s.cardsListHeader}>
-        <p className={s.question}>Question</p>
-        <p className={myCards ? s.answerMyCards : s.answer}>Answer</p>
-        <p className={s.updated}>Last updated</p>
-        <p className={myCards ? s.gradeMyCards : s.grade}>Grade</p>
-        {myCards && <p className={s.actions}>Actions</p>}
-      </div>
+    <>
+      {cards?.length === 0 ? (
+        <NotFound />
+      ) : (
+        <div className={s.cardsList}>
+          <div className={s.cardsListHeader}>
+            <p className={s.question}>Question</p>
+            <p className={myCards ? s.answerMyCards : s.answer}>Answer</p>
+            <p className={s.updated}>Last updated</p>
+            <p className={myCards ? s.gradeMyCards : s.grade}>Grade</p>
+            {myCards && <p className={s.actions}>Actions</p>}
+          </div>
 
-      {isFetching
-        ? skeletons.map(el => (
-            <div className={s.skeletonCardsContainer} key={el}>
-              <Skeleton classes={{ root: s.skeletonCard }} animation="wave" variant="rectangular" />
-            </div>
-          ))
-        : cards?.map(card => (
-            <Card
-              key={card._id}
-              idCard={card._id}
-              question={card.question}
-              grade={card.grade}
-              answer={card.answer}
-              updated={formatDate(card.updated)}
-              userId={card.user_id}
-              questionImg={card.questionImg}
-            />
-          ))}
-    </div>
+          {isFetching
+            ? skeletons.map(el => (
+                <div className={s.skeletonCardsContainer} key={el}>
+                  <Skeleton
+                    classes={{ root: s.skeletonCard }}
+                    animation="wave"
+                    variant="rectangular"
+                  />
+                </div>
+              ))
+            : cards?.map(card => (
+                <Card
+                  key={card._id}
+                  idCard={card._id}
+                  question={card.question}
+                  grade={card.grade}
+                  answer={card.answer}
+                  updated={formatDate(card.updated)}
+                  userId={card.user_id}
+                  questionImg={card.questionImg}
+                />
+              ))}
+        </div>
+      )}
+    </>
   )
 }
