ea8efdfb5041c26213763e6c35534b3416ebc15d
lily1215z <odessa1215@i.ua>
2023-03-01 19:47:33 +0100

    feat: new component for ScrollBtn

--- src/pages/cards-page/CardsPage.tsx
+++ src/pages/cards-page/CardsPage.tsx
@@ -1,31 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 import { useNavigate } from 'react-router-dom'
 
-import { ReactComponent as ArrowUp } from 'assets/img/icons/arrow-up.svg'
+import { BtnScrollUp } from 'components/btnScrollUp/BtnScrollUp'
 import { Cards } from 'modules/cards/Cards'
-import scroll from 'pages/packs-page/PacksPage.module.scss'
-import { Button } from 'UI/button/Button'
 
 export const CardsPage = () => {
   const navigate = useNavigate()
-  const [btnScrollUp, setBtnScrollUp] = useState(false)
-
-  useEffect(() => {
-    const scrollHandler = () => {
-      const top = window.scrollY
-
-      if (top >= 300) {
-        setBtnScrollUp(true)
-      } else {
-        setBtnScrollUp(false)
-      }
-    }
-
-    window.addEventListener('scroll', scrollHandler)
-
-    return () => window.removeEventListener('scroll', scrollHandler)
-  }, [])
 
   return (
     <div>
@@ -39,15 +20,7 @@ export const CardsPage = () => {
 
       <Cards />
 
-      {btnScrollUp && (
-        <Button
-          className={scroll.scrollBtn}
-          styleType="icon"
-          onClick={() => window.scrollTo({ top: 0 })}
-        >
-          <ArrowUp width="19" height="23" />
-        </Button>
-      )}
+      <BtnScrollUp />
     </div>
   )
 }
--- src/pages/packs-page/PacksPage.tsx
+++ src/pages/packs-page/PacksPage.tsx
@@ -1,43 +1,14 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
-import s from './PacksPage.module.scss'
-
-import { ReactComponent as ArrowUp } from 'assets/img/icons/arrow-up.svg'
+import { BtnScrollUp } from 'components/btnScrollUp/BtnScrollUp'
 import { Packs } from 'modules'
-import { Button } from 'UI/button/Button'
 
 export const PacksPage = () => {
-  const [btnScrollUp, setBtnScrollUp] = useState(false)
-
-  useEffect(() => {
-    const scrollHandler = () => {
-      const top = window.scrollY
-
-      if (top >= 300) {
-        setBtnScrollUp(true)
-      } else {
-        setBtnScrollUp(false)
-      }
-    }
-
-    window.addEventListener('scroll', scrollHandler)
-
-    return () => window.removeEventListener('scroll', scrollHandler)
-  }, [])
-
   return (
     <>
       <Packs />
 
-      {btnScrollUp && (
-        <Button
-          className={s.scrollBtn}
-          styleType="icon"
-          onClick={() => window.scrollTo({ top: 0 })}
-        >
-          <ArrowUp width="19" height="23" />
-        </Button>
-      )}
+      <BtnScrollUp />
     </>
   )
 }
