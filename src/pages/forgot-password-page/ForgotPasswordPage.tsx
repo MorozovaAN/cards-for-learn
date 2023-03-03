c15762c37cc96256d9163d73311b4c7182eb0dae
Morozova Anastasiya <MorozovaAnastasiyaN@yandex.ru>
2023-03-03 14:54:00 +0400

    feat: add AnimationWrapper component and tooltip for reset filters button

--- src/pages/forgot-password-page/ForgotPasswordPage.tsx
+++ src/pages/forgot-password-page/ForgotPasswordPage.tsx
@@ -1,22 +1,12 @@
 import React from 'react'
 
-import { AnimatePresence, motion } from 'framer-motion'
-
-import s from './ForgotPasswordPage.module.scss'
-
 import { ForgotPassword } from 'modules'
+import { AnimationWrapper } from 'UI/animation-wrapper/animationWrapper'
 
 export const ForgotPasswordPage = () => {
   return (
-    <AnimatePresence>
-      <motion.div
-        className={s.contentContainer}
-        initial={{ y: -500, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.5 }}
-      >
-        <ForgotPassword />
-      </motion.div>
-    </AnimatePresence>
+    <AnimationWrapper>
+      <ForgotPassword />
+    </AnimationWrapper>
   )
 }
--- src/pages/log-up-page/LogUpPage.tsx
+++ src/pages/log-up-page/LogUpPage.tsx
@@ -1,22 +1,12 @@
 import React from 'react'
 
-import { AnimatePresence, motion } from 'framer-motion'
-
-import s from './LogUpPage.module.scss'
-
 import { LogUp } from 'modules'
+import { AnimationWrapper } from 'UI/animation-wrapper/animationWrapper'
 
 export const LogUpPage = () => {
   return (
-    <AnimatePresence>
-      <motion.div
-        className={s.contentContainer}
-        initial={{ y: -500, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.5 }}
-      >
-        <LogUp />
-      </motion.div>
-    </AnimatePresence>
+    <AnimationWrapper>
+      <LogUp />
+    </AnimationWrapper>
   )
 }
