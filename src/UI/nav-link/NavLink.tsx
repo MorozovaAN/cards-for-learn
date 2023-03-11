5e22d609790daa66615e788f1114bc40eac4091b
Oleg <9helgi9gmail.com>
2023-03-11 12:10:09 +0100

    fix: edit card modal delete: menuHeader add: header links

--- src/UI/nav-link/NavLink.tsx
+++ src/UI/nav-link/NavLink.tsx
@@ -1,6 +1,6 @@
 import React, { FC, ReactNode } from 'react'
 
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import s from './NavLink.module.scss'
 
