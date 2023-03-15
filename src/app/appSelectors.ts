a11f2648cd7099693073e8b1ce90f23cb44d38dd
Morozova Anastasiya <MorozovaAnastasiyaN@yandex.ru>
2023-03-15 17:31:52 +0400

    fix: menu fix

--- src/app/appSelectors.ts
+++ src/app/appSelectors.ts
@@ -6,3 +6,4 @@ export const isLoadingSelector = (state: RootStateType) => state.app.isLoading
 export const notificationSelector = (state: RootStateType) => state.app.notification
 export const modalSelector = (state: RootStateType) => state.app.modal
 export const skeletonsSelector = (state: RootStateType) => state.app.skeletonsNumbers
+export const burgerSelector = (state: RootStateType) => state.app.burger
